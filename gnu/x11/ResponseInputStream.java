297cb45e05f7e83034d1279d7e0f6205b400ae7b
Roman Kennke <roman.kennke@aicas.com>
2006-07-09 19:57:04 +0000

    Fixed reading of events and erros. Stabilized IO for stream based protocol.

--- gnu/x11/ResponseInputStream.java
+++ gnu/x11/ResponseInputStream.java
@@ -208,21 +208,24 @@ public class ResponseInputStream extends FilterInputStream {
     ex.printStackTrace();
   }
 
-  public synchronized Event read_event () {
+  public Event read_event () {
 
     // If there are any events already queued up, then return the first
     // event in the queue.
     if (events.size () > 0) {
-      Event event = (Event) events.poll ();
+      Event event = (Event) events.getFirst ();
       return event;
     }
+
+    //System.err.println("read event from stream");
 
     // Otherwise we read and return the first event from the stream.
     Event ev = null;
     do {
       ev = read_event_from_stream ();
       // If this returned null, there's a reply in the response stream and
-      // some other thread is waiting for it.
+      // some other thread is waiting for it, or there is no event and we keep
+      // waiting for one...
       if (ev == null)
         Thread.yield ();
     } while (ev == null);
@@ -235,129 +238,156 @@ public class ResponseInputStream extends FilterInputStream {
    *
    * @return the next event from the stream
    */
-  private Event read_event_from_stream () {
+  private synchronized Event read_event_from_stream () {
 
-    assert Thread.holdsLock (this);
+    int available = 0;
+    try {
+     available = available ();
+    } catch (IOException ex) {
+      handle_exception (ex);
+    }
+    //System.err.println("bytes available in response stream: " + available);
+    if (available == 0)
+      return null;
 
-      // We want to look-ahead the first byte to determine the type of the
-      // response.
-      int code = -1;
-      try {
-        in.mark (1);
-        code = read_int8 ();
-        in.reset ();
-      } catch (IOException ex) {
-        ex.printStackTrace();
-      }
-      Event ev = null;
-      switch (code) {
-      case 0:
-        read_error ();
-        break;
-      case 1:System.err.println("reply");
-        ev = null;
-        break;
-      case 2:
-        ev = new KeyPress (display, this);
-        break;
-      case 3:
-        ev = new KeyRelease (display, this);
-        break;
-      case 4:
-        ev = new ButtonPress (display, this);
-        break;
-      case 5:
-        ev = new ButtonRelease (display, this);
-        break;
-      case 6:
-        ev = new MotionNotify (display, this);
-        break;
-      case 7:
-        ev = new EnterNotify (display, this);
-        break;
-      case 8:
-        ev = new LeaveNotify (display, this);
-        break;
-      case 9:
-        ev = new FocusIn (display, this);
-        break;
-      case 10:
-        ev = new FocusOut (display, this);
-        break;
-      case 11:
-        ev = new KeymapNotify (display, this);
-        break;
-      case 12:
-        ev = new Expose (display, this);
-        break;
-      case 13:
-        ev = new GraphicsExpose (display, this);
-        break;
-      case 14:
-        ev = new NoExposure (display, this);
-        break;
-      case 15:
-        ev = new VisibilityNotify (display, this);
-        break;
-      case 16:
-        ev = new CreateNotify (display, this);
-        break;
-      case 17:
-        ev = new DestroyNotify (display, this);
-        break;
-      case 18:
-        ev = new UnmapNotify (display, this);
-        break;
-      case 19:
-        ev = new MapNotify (display, this);
-        break;
-      case 20:
-        ev = new MapRequest (display, this);
-        break;
-      case 21:
-        ev = new ReparentNotify (display, this);
-        break;
-      case 22:
-        ev = new ConfigureNotify (display, this);
-        break;
-      case 23: 
-        ev = new ConfigureRequest (display, this);
-        break;
-      case 24:
-        ev = new GravityNotify (display, this);
-        break;
-      case 25:
-        ev = new ResizeRequest (display, this);
-        break;
-      case 26:
-        ev = new CirculateNotify (display, this);
-        break;
-      case 27:
-        ev = new CirculateRequest (display, this);
-        break;
-      case 28:
-        ev = new PropertyNotify (display, this);
-        break;
-      case 29:
-        ev = new SelectionClear (display, this);
-        break;
-      case 30:
-        ev = new SelectionRequest (display, this);
-        break;
-      case 31:
-        ev = new SelectionNotify (display, this);
-        break;
-      case 32:
-        ev = new ColormapNotify (display, this);
-        break;
-      case 33:
-        ev = new ClientMessage (display, this);
-        break;
-      case 34:
-        ev = new MappingNotify (display, this);
-        break;
-      }
-      return ev;
+    // We want to look-ahead the first byte to determine the type of the
+    // response.
+    int code = -1;
+    try {
+      in.mark (1);
+      code = read_int8 ();
+      in.reset ();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    //System.err.println("reading code: " + code + " masked: " + (code & 0x7f));
+    code = code & 0x7f; // Remove synthetic mask.
+    Event ev = null;
+    if (code >= 64 && code <= 127)
+      ev = read_extension_event (code);
+    else
+      ev = read_core_event (code);
+    return ev;
+  }
+
+  private Event read_core_event (int code) {
+    Event ev = null;
+    switch (code) {
+    case 0:
+      read_error ();
+      break;
+    case 1:
+      ev = null;
+      break;
+    case 2:
+      ev = new KeyPress (display, this);
+      break;
+    case 3:
+      ev = new KeyRelease (display, this);
+      break;
+    case 4:
+      ev = new ButtonPress (display, this);
+      break;
+    case 5:
+      ev = new ButtonRelease (display, this);
+      break;
+    case 6:
+      ev = new MotionNotify (display, this);
+      break;
+    case 7:
+      ev = new EnterNotify (display, this);
+      break;
+    case 8:
+      ev = new LeaveNotify (display, this);
+      break;
+    case 9:
+      ev = new FocusIn (display, this);
+      break;
+    case 10:
+      ev = new FocusOut (display, this);
+      break;
+    case 11:
+      ev = new KeymapNotify (display, this);
+      break;
+    case 12:
+      ev = new Expose (display, this);
+      break;
+    case 13:
+      ev = new GraphicsExpose (display, this);
+      break;
+    case 14:
+      ev = new NoExposure (display, this);
+      break;
+    case 15:
+      ev = new VisibilityNotify (display, this);
+      break;
+    case 16:
+      ev = new CreateNotify (display, this);
+      break;
+    case 17:
+      ev = new DestroyNotify (display, this);
+      break;
+    case 18:
+      ev = new UnmapNotify (display, this);
+      break;
+    case 19:
+      ev = new MapNotify (display, this);
+      break;
+    case 20:
+      ev = new MapRequest (display, this);
+      break;
+    case 21:
+      ev = new ReparentNotify (display, this);
+      break;
+    case 22:
+      ev = new ConfigureNotify (display, this);
+      break;
+    case 23: 
+      ev = new ConfigureRequest (display, this);
+      break;
+    case 24:
+      ev = new GravityNotify (display, this);
+      break;
+    case 25:
+      ev = new ResizeRequest (display, this);
+      break;
+    case 26:
+      ev = new CirculateNotify (display, this);
+      break;
+    case 27:
+      ev = new CirculateRequest (display, this);
+      break;
+    case 28:
+      ev = new PropertyNotify (display, this);
+      break;
+    case 29:
+      ev = new SelectionClear (display, this);
+      break;
+    case 30:
+      ev = new SelectionRequest (display, this);
+      break;
+    case 31:
+      ev = new SelectionNotify (display, this);
+      break;
+    case 32:
+      ev = new ColormapNotify (display, this);
+      break;
+    case 33:
+      ev = new ClientMessage (display, this);
+      break;
+    case 34:
+      ev = new MappingNotify (display, this);
+      break;
+    default:
+      throw new java.lang.Error ("Unsupported core event code: " + code);
+    }
+    return ev;
+  }
+
+  private Event read_extension_event (int code) {
+    // FIXME: Implement extension events.
+    System.err.println("implement extension events: " + code);
+    return null;
   }
 
   /**
@@ -378,31 +408,45 @@ public class ResponseInputStream extends FilterInputStream {
     assert Thread.holdsLock (out);
 
     int exp_seq_no = out.seq_number;
+    //System.err.println("reading reply for: " + out.buffer[0] + " seq_no: " + exp_seq_no);
 
     // Flush the current request.
     out.flush();
 
-    // Fetch all events that may come before the reply.
-    Event ev = null;
+    // Fetch all events and errors that may come before the reply.
+    int code = -1;
     do {
-      ev = read_event_from_stream ();
-      System.err.println("queueing event: " + ev);
-      if (ev != null)
-        events.offer (ev);
-    } while (ev != null);
+      try {
+        mark (1);
+        code = read_int8 ();
+        reset ();
+      } catch (IOException ex) {
+        handle_exception (ex);
+      }
+      if (code == 0) // Error.
+        read_error ();
+      else if (code > 1) { // Event.
+        Event ev = read_event_from_stream ();
+        if (ev != null)
+          events.addLast (ev);
+      }// else // Reply or Exception.
+    } while (code != 1);
 
+    // Check reply header, especially make sure that the sequence codes match.
     try {
       mark (4);
       int reply = read_int8 ();
-      assert reply == 1;
+      assert reply == 1 : "Reply code must be 1 but is: " + reply;
       skip (1);
       int seq_no = read_int16 ();
       assert exp_seq_no == seq_no : "expected sequence number: " + exp_seq_no
-      + " got sequence number: " + seq_no;
+                           + " got sequence number: " + seq_no;
       reset ();
     } catch (IOException ex) {
       handle_exception (ex);
     }
+
+    // Now the calling thread can safely read the reply.
   }
 
 
@@ -410,7 +454,18 @@ public class ResponseInputStream extends FilterInputStream {
    * Reads an X error from the stream.
    */
   private void read_error () {
-    
+    int reply = read_int8 ();
+    assert reply == 0;
+    int code = read_int8 ();
+    int seq_no = read_int16 ();
+    int bad_value = read_int32 ();
+    int minor_opcode = read_int16 ();
+    int major_opcode = read_int8 ();
+    skip (21);
+    gnu.x11.Error err = new gnu.x11.Error (display,
+                                           "Error", //gnu.x11.Error.ERROR_STRINGS [code],
+                                           code, seq_no, bad_value,
+                                           minor_opcode, major_opcode);
+    throw err;
   }
-
 }
