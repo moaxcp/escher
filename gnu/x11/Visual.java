6ae300f5d56f76ae37081b607bdca9a1e0dabb83
Roman Kennke <roman.kennke@aicas.com>
2006-07-09 19:59:00 +0000

    Use new stream based protocol impl.

--- gnu/x11/Visual.java
+++ gnu/x11/Visual.java
@@ -2,20 +2,31 @@ package gnu.x11;
 
 
 /** X visual. */
-public class Visual extends Data {
-  public static final Visual COPY_FROM_PARENT = new Visual (4);
+public class Visual {
 
+  public static final Visual COPY_FROM_PARENT = new Visual ();
 
-  public Visual (int length) { super (length); }
-  public Visual (Data data, int offset) { super (data, offset); }
-  public int id () { return read4 (0); }
-  public int bits_per_rgb_value () { return read1 (5); }
-  public int colormap_entries () { return read2 (6); }
-  public int red_mask () { return read4 (8); }
-  public int green_mask () { return read4 (12); }
-  public int blue_mask () { return read4 (16); }
+  public int id;
+  public int visual_class;
+  public int bits_per_rgb_value;
+  public int colormap_entries;
+  public int red_mask;
+  public int green_mask;
+  public int blue_mask;
 
+  private Visual () {}
 
+  Visual (ResponseInputStream in) {
+    id = in.read_int32 ();
+    visual_class = in.read_int8 ();
+    bits_per_rgb_value = in.read_int8 ();
+    colormap_entries = in.read_int16();
+    red_mask = in.read_int32 ();
+    green_mask = in.read_int32 ();
+    blue_mask = in.read_int32 ();
+    in.skip (4);
+
+  }
   public static final int STATIC_GRAY = 0;
   public static final int GRAY_SCALE = 1;
   public static final int STATIC_COLOR = 2;
@@ -29,27 +40,14 @@ public class Visual extends Data {
     "true-color", "direct-color" 
   };
 
-
-  /**
-   * @return valid:
-   * {@link #STATIC_GRAY}
-   * {@link #STATIC_COLOR}
-   * {@link #TRUE_COLOR}
-   * {@link #GRAY_SCALE}
-   * {@link #PSEUDO_COLOR}
-   * {@link #DIRECT_COLOR}
-   */
-  public int klass () { return read1 (4); }
-
-
   public String toString () {
     return "#Visual"
-      + "\n  id: " + id ()
-      + "\n  class: " + CLASS_STRINGS [klass ()]
-      + "\n  bits-per-rgb-value: " + bits_per_rgb_value ()
-      + "\n  colormap-entries: " + colormap_entries ()
-      + "\n  red-mask: 0x" + Integer.toHexString (red_mask ())
-      + "\n  green-mask: 0x " + Integer.toHexString (green_mask ())
-      + "\n  blue-mask: 0x" + Integer.toHexString (blue_mask ());
+      + "\n  id: " + id
+      + "\n  class: " + CLASS_STRINGS [visual_class]
+      + "\n  bits-per-rgb-value: " + bits_per_rgb_value
+      + "\n  colormap-entries: " + colormap_entries
+      + "\n  red-mask: 0x" + Integer.toHexString (red_mask)
+      + "\n  green-mask: 0x " + Integer.toHexString (green_mask)
+      + "\n  blue-mask: 0x" + Integer.toHexString (blue_mask);
   }
 }
