cf52dba14446d18090936e57ee70bc0b6338c642
Marcos Roriz <marcosrorizinf@gmail.com>
2010-05-27 16:49:58 -0300

    Cursor refactoration

--- src/gnu/x11/Cursor.java
+++ src/gnu/x11/Cursor.java
@@ -11,101 +11,115 @@ public class Cursor extends Resource {
   public static final Cursor NONE = new Cursor (0);
   public static final Cursor CURRENT = new Cursor (1);
 
+  /**
+   * This enum represents the shape of a Cursor.
+   */
+  public enum Shape {
+    X_CURSOR(0), 
+    ARROW(2),
+    BASED_ARROW_DOWN(4),
+    BASED_ARROW_UP(6),
+    BOAT(8),
+    BOGOSITY(10),
+    BOTTOM_LEFT_CORNER(12),
+    BOTTOM_RIGHT_CORNER(14),
+    BOTTOM_SIDE(16),
+    BOTTOM_TEE(18),
+    BOX_SPIRAL(20),
+    CENTER_PTR(22),
+    CIRCLE(24),
+    CLOCK(26),
+    COFFEE_MUG(28),
+    CROSS(30),
+    CROSS_REVERSE(32),
+    CROSSHAIR(34),
+    DIAMOND_CROSS(36),
+    DOT(38),
+    DOTBOX(40),
+    DOUBLE_ARROW(42),
+    DRAFT_LARGE(44),
+    DRAFT_SMALL(46),
+    DRAPED_BOX(48),
+    EXCHANGE(50),
+    FLEUR(52),
+    GOBBLER(54),
+    GUMBY(56),
+    HAND1(58),
+    HAND2(60),
+    HEART(62),
+    ICON(64),
+    IRON_CROSS(66),
+    LEFT_PTR(68),
+    LEFT_SIDE(70),
+    LEFT_TEE(72),
+    LEFTBUTTON(74),
+    LL_ANGLE(76),
+    LR_ANGLE(78),
+    MAN(80),
+    MIDDLEBUTTON(82),
+    MOUSE(84),
+    PENCIL(86),
+    PIRATE(88),
+    PLUS(90),
+    QUESTION_ARROW(92),
+    RIGHT_PTR(94),
+    RIGHT_SIDE(96),
+    RIGHT_TEE(98),
+    RIGHTBUTTON(100),
+    RTL_LOGO(102),
+    SAILBOAT(104),
+    SB_DOWN_ARROW(106),
+    SB_H_DOUBLE_ARROW(108),
+    SB_LEFT_ARROW(110),
+    SB_RIGHT_ARROW(112),
+    SB_UP_ARROW(114),
+    SB_V_DOUBLE_ARROW(116),
+    SHUTTLE(118),
+    SIZING(120),
+    SPIDER(122),
+    SPRAYCAN(124),
+    STAR(126),
+    TARGET(128),
+    TCROSS(130),
+    TOP_LEFT_ARROW(132),
+    TOP_LEFT_CORNER(134),
+    TOP_RIGHT_CORNER(136),
+    TOP_SIDE(138),
+    TOP_TEE(140),
+    TREK(142),
+    UL_ANGLE(144),
+    UMBRELLA(146),
+    UR_ANGLE(148),
+    WATCH(150),
+    XTERM(152);
+    
+    private int cursorID;
 
-  // cursor shapes
-  public static final int X_CURSOR = 0;
-  public static final int ARROW = 2;
-  public static final int BASED_ARROW_DOWN = 4;
-  public static final int BASED_ARROW_UP = 6;
-  public static final int BOAT = 8;
-  public static final int BOGOSITY = 10;
-  public static final int BOTTOM_LEFT_CORNER = 12;
-  public static final int BOTTOM_RIGHT_CORNER = 14;
-  public static final int BOTTOM_SIDE = 16;
-  public static final int BOTTOM_TEE = 18;
-  public static final int BOX_SPIRAL = 20;
-  public static final int CENTER_PTR = 22;
-  public static final int CIRCLE = 24;
-  public static final int CLOCK = 26;
-  public static final int COFFEE_MUG = 28;
-  public static final int CROSS = 30;
-  public static final int CROSS_REVERSE = 32;
-  public static final int CROSSHAIR = 34;
-  public static final int DIAMOND_CROSS = 36;
-  public static final int DOT = 38;
-  public static final int DOTBOX = 40;
-  public static final int DOUBLE_ARROW = 42;
-  public static final int DRAFT_LARGE = 44;
-  public static final int DRAFT_SMALL = 46;
-  public static final int DRAPED_BOX = 48;
-  public static final int EXCHANGE = 50;
-  public static final int FLEUR = 52;
-  public static final int GOBBLER = 54;
-  public static final int GUMBY = 56;
-  public static final int HAND1 = 58;
-  public static final int HAND2 = 60;
-  public static final int HEART = 62;
-  public static final int ICON = 64;
-  public static final int IRON_CROSS = 66;
-  public static final int LEFT_PTR = 68;
-  public static final int LEFT_SIDE = 70;
-  public static final int LEFT_TEE = 72;
-  public static final int LEFTBUTTON = 74;
-  public static final int LL_ANGLE = 76;
-  public static final int LR_ANGLE = 78;
-  public static final int MAN = 80;
-  public static final int MIDDLEBUTTON = 82;
-  public static final int MOUSE = 84;
-  public static final int PENCIL = 86;
-  public static final int PIRATE = 88;
-  public static final int PLUS = 90;
-  public static final int QUESTION_ARROW = 92;
-  public static final int RIGHT_PTR = 94;
-  public static final int RIGHT_SIDE = 96;
-  public static final int RIGHT_TEE = 98;
-  public static final int RIGHTBUTTON = 100;
-  public static final int RTL_LOGO = 102;
-  public static final int SAILBOAT = 104;
-  public static final int SB_DOWN_ARROW = 106;
-  public static final int SB_H_DOUBLE_ARROW = 108;
-  public static final int SB_LEFT_ARROW = 110;
-  public static final int SB_RIGHT_ARROW = 112;
-  public static final int SB_UP_ARROW = 114;
-  public static final int SB_V_DOUBLE_ARROW = 116;
-  public static final int SHUTTLE = 118;
-  public static final int SIZING = 120;
-  public static final int SPIDER = 122;
-  public static final int SPRAYCAN = 124;
-  public static final int STAR = 126;
-  public static final int TARGET = 128;
-  public static final int TCROSS = 130;
-  public static final int TOP_LEFT_ARROW = 132;
-  public static final int TOP_LEFT_CORNER = 134;
-  public static final int TOP_RIGHT_CORNER = 136;
-  public static final int TOP_SIDE = 138;
-  public static final int TOP_TEE = 140;
-  public static final int TREK = 142;
-  public static final int UL_ANGLE = 144;
-  public static final int UMBRELLA = 146;
-  public static final int UR_ANGLE = 148;
-  public static final int WATCH = 150;
-  public static final int XTERM = 152;
-
-
+    private Shape(int id) {
+      this.cursorID = id;
+    }
+    
+    public int getShapeID() {
+      return this.cursorID;
+    }
+    
+    public String getShapeName() {
+      return this.name();
+    }
+  }
+  
   // cursor font
   public static Font FONT = null;
-  
 
   /** Predefined. */
   public Cursor (int id) {
     super (id);
   }
 
-
   /**
    * @see <a href="XCreateFontCursor.html">XCreateFontCursor</a>
    */  
-  public Cursor (Display display, int shape) {
+  public Cursor (Display display, Shape shape) {
     super (display);
     
     // X predefined special font
@@ -117,32 +131,33 @@ public class Cursor extends Resource {
      */
 
     // black and white
-    create (FONT, FONT, shape, shape+1, 0, 0, 0, 1, 1, 1);
+    create (FONT, FONT, shape.getShapeID(), shape.getShapeID() + 1, 0, 0, 0, 1, 1, 1);
   }
 
 
   /**
    * @see <a href="XCreateGlyphCursor.html">XCreateGlyphCursor</a>
    */
-  public Cursor (Font src, Font mask, int source_char,  
-    int mask_char, int fg_r, int fg_g, int fg_b, 
+  public Cursor (Font src, Font mask, Shape source_char,  
+    Shape mask_char, int fg_r, int fg_g, int fg_b, 
     int bg_r, int bg_g, int bg_b) { 
 
     super (src.display);
-    create (src, mask, source_char, mask_char, fg_r, fg_g, fg_b,
+    create (src, mask, source_char.getShapeID(), mask_char.getShapeID(), fg_r, fg_g, fg_b,
       bg_r, bg_g, bg_b);
   }
 
 
   // opcode 93 - create cursor
+  //@Question The variables mask_char and source_char aren't used here, do we need them?
+  //@Question Or they are here to make 'compatible' with the X protocol?
   /**
    * @param mask possible: {@link Pixmap#NONE}
    * @see <a href="XCreatePixmapCursor.html">XCreatePixmapCursor</a>
    */
-  public Cursor (Pixmap src, Pixmap mask, int source_char,  
-                 int mask_char, int fg_r, int fg_g, int fg_b, 
+  public Cursor (Pixmap src, Pixmap mask, Shape source_char,  
+                 Shape mask_char, int fg_r, int fg_g, int fg_b, 
                  int bg_r, int bg_g, int bg_b, int x, int y) { 
-
     super (src.display);
 
     RequestOutputStream o = display.out;
