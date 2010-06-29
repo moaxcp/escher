852bb806736bfb563fae3b08e114662b2e56f62e
Marcos Roriz <marcosrorizinf@gmail.com>
2010-06-29 15:58:29 -0300

    Extracted these two classes

---
+++ src/gnu/x11/Host.java
@@ -0,0 +1,134 @@
+package gnu.x11;
+
+
+public class Host {
+    
+    private InternetFamily family;
+    private byte[] address;
+    
+    public enum InternetFamily {
+        INTERNET(0),
+        DECNET(1),
+        CHAOS(2);
+        
+        private int code;
+        
+        InternetFamily(int cd) {
+            this.code =cd;
+        }
+        
+        public int getCode() {
+            return code;
+        }
+        
+        public static InternetFamily getFamily(int code) {
+            switch (code)
+            {
+                case 0: return InternetFamily.INTERNET;
+                case 1: return InternetFamily.DECNET;
+                case 2: return InternetFamily.CHAOS;
+                
+                default: return InternetFamily.INTERNET;
+            }
+        }
+    }
+    
+    public enum ChangeOperation {
+        INSERT(0),
+        DELETE(1);
+        
+        private int code;
+        
+        ChangeOperation(int cd) {
+            this.code =cd;
+        }
+        
+        public int getCode() {
+            return code;
+        }
+        
+        public static ChangeOperation getOperation(int code) {
+            return code == 0 ? ChangeOperation.INSERT : ChangeOperation.DELETE;
+        }
+    }
+    
+    public enum ForceScreenSaver {
+        ACTIVATE(0),
+        RESET(1);
+        
+        private int code;
+        
+        ForceScreenSaver(int cd) {
+            this.code =cd;
+        }
+        
+        public int getCode() {
+            return code;
+        }
+        
+        public static ForceScreenSaver getOperation(int code) {
+            return code == 0 ? ForceScreenSaver.ACTIVATE : ForceScreenSaver.RESET;
+        }
+    }
+
+    public enum AccessControl {
+        ENABLE(0),
+        DISABLED(1);
+        
+        private int code;
+        
+        AccessControl(int cd) {
+            this.code =cd;
+        }
+        
+        public int getCode() {
+            return code;
+        }
+        
+        public static AccessControl getControl(boolean code) {
+            return code ? AccessControl.ENABLE : AccessControl.DISABLED;
+        }
+    }
+    
+    public enum CloseDownMethod {
+        DESTROY(0),
+        RETAIN_PERMANENT(1),
+        RETAIN_TEMPORARY(2);
+        
+        private int code;
+        
+        CloseDownMethod(int cd) {
+            this.code =cd;
+        }
+        
+        public int getCode() {
+            return code;
+        }
+        
+        public static CloseDownMethod getFamily(int code) {
+            switch (code)
+            {
+                case 0: return CloseDownMethod.DESTROY;
+                case 1: return CloseDownMethod.RETAIN_PERMANENT;
+                case 2: return CloseDownMethod.RETAIN_TEMPORARY;
+                
+                default: return CloseDownMethod.DESTROY;
+            }
+        }
+    }
+    
+    /**
+     * Reads one Host instance from a ResponseInputStream.
+     * 
+     * @param in
+     *            the input stream to read from
+     */
+    Host(ResponseInputStream in) {
+        family = InternetFamily.getFamily(in.readInt8());
+        in.skip(1);
+        int addLen = in.readInt16();
+        address = new byte[addLen];
+        in.readData(address);
+        in.pad(addLen);
+    }
+}
