577f1f5947452d58161b3552e322224c8485846a
Marcos Roriz <marcosrorizinf@gmail.com>
2010-07-14 17:30:41 -0300

    Shape: * Added some enums * Added accessors (getters/setters) * Added fixed member name to camelCase

--- src/gnu/x11/extension/Shape.java
+++ src/gnu/x11/extension/Shape.java
@@ -16,7 +16,7 @@ import gnu.x11.event.Event;
  * >protocol</a>).
  */
 public class Shape extends Extension implements EventFactory {
-  public static final String [] MINOR_OPCODE_STRINGS = {
+  private static final String[] MINOR_OPCODE_STRINGS = {
     "QueryVersion",             // 0
     "Rectangles",               // 1
     "Mask",                     // 2
@@ -33,164 +33,192 @@ public class Shape extends Extension implements EventFactory {
   public static final int CLIENT_MINOR_VERSION = 0;
 
 
-  public int server_major_version, server_minor_version;
+  private int serverMajorVersion, serverMinorVersion;
 
 
   // shape opcode 0 - query version
   /**
    * @see <a href="XShapeQueryExtension.html">XShapeQueryExtension</a>
    */
-  public Shape (Display display) throws NotFoundException { 
-    super (display, "SHAPE", MINOR_OPCODE_STRINGS, 0, 1);
+  public Shape(Display display) throws NotFoundException { 
+    super(display, "SHAPE", MINOR_OPCODE_STRINGS, 0, 1);
 
     // check version before any other operations
     RequestOutputStream o = display.getResponseOutputStream();
-    synchronized (o) {
-      o.beginRequest (majorOpcode, 0, 1);
+    synchronized(o) {
+      o.beginRequest(majorOpcode, 0, 1);
       ResponseInputStream i = display.getResponseInputStream();
-      synchronized (i) {
-        i.readReply (o);
-        i.skip (8);
-        server_major_version = i.readInt16 ();
-        server_minor_version = i.readInt16 ();
-        i.skip (20);
+      synchronized(i) {
+        i.readReply(o);
+        i.skip(8);
+        serverMajorVersion = i.readInt16();
+        serverMinorVersion = i.readInt16();
+        i.skip(20);
       }
     }
   }
 
-  public static final int BOUNDING = 0;
-  public static final int CLIP = 1;
+  public enum Kind {
+      BOUNDING(0),
+      CLIP(1);
+      
+      private int code;
+      
+      Kind(int code) {
+          this.code = code;
+      }
+      
+      public int getCode() {
+          return this.code;
+      }
+      
+      public static Kind getByCode(int code) {
+          return code == 0 ? BOUNDING : CLIP;
+      }
+  }
 
+  public enum Operation {
+      SET(0),
+      UNION(1),
+      INTERSECT(2),
+      SUBTRACT(3),
+      INVERT(4);
+      
+      private int code;
+      
+      Operation(int code) {
+          this.code = code;
+      }
+      
+      public int getCode() {
+          return this.code;
+      }
+  }
 
-  public static final int SET = 0;
-  public static final int UNION = 1;
-  public static final int INTERSECT = 2;
-  public static final int SUBTRACT = 3;
-  public static final int INVERT = 4;
-
-
-  public static final int UN_SORTED = 0;
-  public static final int Y_SORTED = 1;
-  public static final int YX_SORTED = 2;
-  public static final int YX_BANDED = 3;
-
+  public enum Ordering {
+      UN_SORTED(0),
+      Y_SORTED(1),
+      YX_SORTED(2),
+      YX_BANDED(3);
+      
+      private int code;
+      
+      Ordering(int code) {
+          this.code = code;
+      }
+      
+      public int getCode() {
+          return this.code;
+      }
+      
+      public static Ordering getByCode(int code) {
+          switch (code) {
+              case 0: return UN_SORTED;
+              case 1: return Y_SORTED;
+              case 2: return YX_SORTED;
+              case 3: return YX_BANDED;
+              default: return UN_SORTED;
+          }
+      }
+  }
 
   // shape opcode 1 - rectangles
   /**
-   * @param dest_kind valid:
-   * {@link #BOUNDING},
-   * {@link #CLIP}
+   * @param destKind valid:
+   * {@link Kind}
    * 
    * @param operation valid:
-   * {@link #SET},
-   * {@link #UNION},
-   * {@link #INTERSECT},
-   * {@link #SUBTRACT},
-   * {@link #INVERT}
+   * {@link Operation}
    * 
    * @param ordering valid: 
-   * {@link #UN_SORTED},
-   * {@link #Y_SORTED},
-   * {@link #YX_SORTED},
-   * {@link #YX_BANDED}
+   * {@link Ordering}
    * 
    * @see <a href="XShapeCombineRectangles.html">XShapeCombineRectangles</a>
    */
-  public void combine_rectangles (Window dest, int dest_kind, int x_offset,
-                                  int y_offset, Rectangle [] rectangles,
-                                  int operation, int ordering) {
+  public void combineRectangles(Window dest, Kind destKind, int xOffset,
+                                  int yOffset, Rectangle[] rectangles,
+                                  Operation operation, Ordering ordering) {
 
     RequestOutputStream o = display.getResponseOutputStream();
-    synchronized (o) {
-      o.beginRequest (majorOpcode, 1, 4 + 2 * rectangles.length);
-      o.writeInt8 (operation);
-      o.writeInt8 (dest_kind);
-      o.writeInt8 (ordering);
-      o.skip (1);
-      o.writeInt32 (dest.getID());
-      o.writeInt16 (x_offset);
-      o.writeInt16 (y_offset);
+    synchronized(o) {
+      o.beginRequest(majorOpcode, 1, 4 + 2 * rectangles.length);
+      o.writeInt8(operation.getCode());
+      o.writeInt8(destKind.getCode());
+      o.writeInt8(ordering.getCode());
+      o.skip(1);
+      o.writeInt32(dest.getID());
+      o.writeInt16(xOffset);
+      o.writeInt16(yOffset);
 
-      for (int i = 0; i < rectangles.length; i++) {
-        o.writeInt16 (rectangles [i].getX());
-        o.writeInt16 (rectangles [i].getY());
-        o.writeInt16 (rectangles [i].getWidth());
-        o.writeInt16 (rectangles [i].getHeight());
+      for (Rectangle rect : rectangles) {
+        o.writeInt16(rect.getX());
+        o.writeInt16(rect.getY());
+        o.writeInt16(rect.getWidth());
+        o.writeInt16(rect.getHeight());
       }
 
-      o.send ();
+      o.send();
     }
   }
 
 
   // shape opcode 2 - mask
   /**
-   * @param dest_kind valid:
-   * {@link #BOUNDING},
-   * {@link #CLIP}
+   * @param destKind valid:
+   * {@link Kind},
    * 
    * @param operation valid:
-   * {@link #SET},
-   * {@link #UNION},
-   * {@link #INTERSECT},
-   * {@link #SUBTRACT},
-   * {@link #INVERT}
+   * {@link Operation}
    * 
    * @param src possible: {@link gnu.x11.Pixmap#NONE}
    * @see <a href="XShapeCombineMask.html">XShapeCombineMask</a>
    */
-  public void combine_mask (Window dest, int dest_kind, int x_offset, 
-                            int y_offset, Pixmap src, int operation) { 
+  public void combineMask(Window dest, Kind destKind, int xOffset, 
+                            int yOffset, Pixmap src, Operation operation) { 
 
     RequestOutputStream o = display.getResponseOutputStream();
-    synchronized (o) {
-      o.beginRequest (majorOpcode, 2, 5);
-      o.writeInt8 (operation);
-      o.writeInt8 (dest_kind);
-      o.skip (2);
-      o.writeInt32 (dest.getID());
-      o.writeInt16 (x_offset);
-      o.writeInt16 (y_offset);
-      o.writeInt32 (src.getID());
-      o.send ();
+    synchronized(o) {
+      o.beginRequest(majorOpcode, 2, 5);
+      o.writeInt8(operation.getCode());
+      o.writeInt8(destKind.getCode());
+      o.skip(2);
+      o.writeInt32(dest.getID());
+      o.writeInt16(xOffset);
+      o.writeInt16(yOffset);
+      o.writeInt32(src.getID());
+      o.send();
     }
   }
 
 
   // shape opcode 3 - combine
   /**
-   * @param dest_kind valid:
-   * {@link #BOUNDING},
-   * {@link #CLIP}
+   * @param destKind valid:
+   * {@link Kind}
    * 
-   * @param src_kind valid: 
-   * {@link #BOUNDING},
-   * {@link #CLIP}
+   * @param srcKind valid: 
+   * {@link Kind}
    * 
    * @param operation valid:
-   * {@link #SET},
-   * {@link #UNION},
-   * {@link #INTERSECT},
-   * {@link #SUBTRACT},
-   * {@link #INVERT}
+   * {@link Operation}
    * 
    * @see <a href="XShapeCombineShape.html">XShapeCombineShape</a>
    */
-  public void combine_shape (Window dest, int dest_kind, int x_offset,
-                             int y_offset, Window src, int src_kind, int operation) { 
+  public void combineShape(Window dest, Kind destKind, int xOffset, int yOffset,
+                           Window src, Kind srcKind, Operation operation) { 
 
     RequestOutputStream o = display.getResponseOutputStream();
-    synchronized (o) {
-      o.beginRequest (majorOpcode, 3, 5);
-      o.writeInt8 (operation);
-      o.writeInt8 (dest_kind);
-      o.writeInt8 (src_kind);
-      o.skip (1);
-      o.writeInt32 (dest.getID());
-      o.writeInt16 (x_offset);
-      o.writeInt16 (y_offset);
-      o.writeInt32 (src.getID());
-      o.send ();
+    synchronized(o) {
+      o.beginRequest(majorOpcode, 3, 5);
+      o.writeInt8(operation.getCode());
+      o.writeInt8(destKind.getCode());
+      o.writeInt8(srcKind.getCode());
+      o.skip(1);
+      o.writeInt32(dest.getID());
+      o.writeInt16(xOffset);
+      o.writeInt16(yOffset);
+      o.writeInt32(src.getID());
+      o.send();
     }
   }
 
@@ -199,18 +227,17 @@ public class Shape extends Extension implements EventFactory {
   /**
    * @see <a href="XShapeOffsetShape.html">XShapeOffsetShape</a>
    */
-  public void offset_shape (Window dest, int dest_kind,
-                            int x_offset, int y_offset) {
+  public void offsetShape(Window dest, Kind destKind, int xOffset, int yOffset) {
 
     RequestOutputStream o = display.getResponseOutputStream();
-    synchronized (o) {
-      o.beginRequest (majorOpcode, 4, 4);
-      o.writeInt8 (dest_kind);
-      o.skip (3);
-      o.writeInt32 (dest.getID());
-      o.writeInt16 (x_offset);
-      o.writeInt16 (y_offset);
-      o.send ();
+    synchronized(o) {
+      o.beginRequest(majorOpcode, 4, 4);
+      o.writeInt8(destKind.getCode());
+      o.skip(3);
+      o.writeInt32(dest.getID());
+      o.writeInt16(xOffset);
+      o.writeInt16(yOffset);
+      o.send();
     }
   }
 
@@ -218,50 +245,46 @@ public class Shape extends Extension implements EventFactory {
   /** Reply of {@link #extents(Window)} */
   public static class ExtentsInfo {
 
-    public boolean bounding_shaped;
-    public boolean clip_shaped;
-    public int bounding_x;
-    public int bounding_y;
-    public int bounding_width;
-    public int bounding_height;
-    public int clip_x;
-    public int clip_y;
-    public int clip_width;
-    public int clip_height;
+    private boolean boundingShaped;
+    private boolean clipShaped;
+    private int boundingX;
+    private int boundingY;
+    private int boundingWidth;
+    private int boundingHeight;
+    private int clipX;
+    private int clipY;
+    private int clipWidth;
+    private int clipHeight;
     
-    ExtentsInfo (ResponseInputStream i) {
-      bounding_shaped = i.readBool ();
-      clip_shaped = i.readBool ();
-      i.skip (2);
-      bounding_x = i.readInt16 ();
-      bounding_y = i.readInt16 ();
-      bounding_width = i.readInt16 ();
-      bounding_height = i.readInt16 ();
-      clip_x = i.readInt16 ();
-      clip_y = i.readInt16 ();
-      clip_width = i.readInt16 ();
-      clip_height = i.readInt16 ();
+    ExtentsInfo(ResponseInputStream i) {
+      boundingShaped = i.readBool();
+      clipShaped = i.readBool();
+      i.skip(2);
+      boundingX = i.readInt16();
+      boundingY = i.readInt16();
+      boundingWidth = i.readInt16();
+      boundingHeight = i.readInt16();
+      clipX = i.readInt16();
+      clipY = i.readInt16();
+      clipWidth = i.readInt16();
+      clipHeight = i.readInt16();
     }
   
-    public Rectangle bounding () { 
-      return new Rectangle (
-        bounding_x, bounding_y,
-        bounding_width, bounding_height);
+    public Rectangle bounding() { 
+      return new Rectangle(boundingX, boundingY, boundingWidth, boundingHeight);
     }
         
   
-    public Rectangle clip () { 
-      return new Rectangle (
-        clip_x, clip_y,
-        clip_width, clip_height);
+    public Rectangle clip() { 
+      return new Rectangle(clipX, clipY, clipWidth, clipHeight);
     }
         
   
-    public String toString () {
+    public String toString() {
       String output = "#Extents ";
   
-      if (bounding_shaped) output += "Bounding: " + bounding ();
-      if (clip_shaped) output += "Clip: " + clip ();
+      if (boundingShaped) output += "Bounding: " + bounding();
+      if (clipShaped) output += "Clip: " + clip();
   
       return output;
     }
@@ -272,19 +295,19 @@ public class Shape extends Extension implements EventFactory {
   /**
    * @see <a href="XShapeQueryExtents.html">XShapeQueryExtents</a>
    */
-  public ExtentsInfo extents (Window dest) {
+  public ExtentsInfo extents(Window dest) {
     
     ExtentsInfo info;
     RequestOutputStream o = display.getResponseOutputStream();
-    synchronized (o) {
-      o.beginRequest (majorOpcode, 5, 2);
-      o.writeInt32 (dest.getID());
+    synchronized(o) {
+      o.beginRequest(majorOpcode, 5, 2);
+      o.writeInt32(dest.getID());
       ResponseInputStream i = display.getResponseInputStream();
-      synchronized (i) {
-        i.readReply (o);
-        i.skip (8);
-        info = new ExtentsInfo (i);
-        i.skip (4);
+      synchronized(i) {
+        i.readReply(o);
+        i.skip(8);
+        info = new ExtentsInfo(i);
+        i.skip(4);
       }
     }
     return info;
@@ -295,14 +318,14 @@ public class Shape extends Extension implements EventFactory {
   /**
    * @see <a href="XShapeSelectInput.html">XShapeSelectInput</a>
    */
-  public void select_input (Window dest, boolean enable) {
+  public void selectInput(Window dest, boolean enable) {
 
     RequestOutputStream o = display.getResponseOutputStream();
-    synchronized (o) {
-      o.beginRequest (majorOpcode, 6, 3);
-      o.writeInt32 (dest.getID());
-      o.writeBool (enable);
-      o.send ();    
+    synchronized(o) {
+      o.beginRequest(majorOpcode, 6, 3);
+      o.writeInt32(dest.getID());
+      o.writeBool(enable);
+      o.send();    
     }
   }
 
@@ -311,18 +334,18 @@ public class Shape extends Extension implements EventFactory {
   /**
    * @see <a href="XShapeInputSelected.html">XShapeInputSelected</a>
    */
-  public boolean input_selected (Window dest) {
+  public boolean input_selected(Window dest) {
     boolean enabled;
     RequestOutputStream o = display.getResponseOutputStream();
-    synchronized (o) {
-      o.beginRequest (majorOpcode, 6, 2);
-      o.writeInt32 (dest.getID());
+    synchronized(o) {
+      o.beginRequest(majorOpcode, 6, 2);
+      o.writeInt32(dest.getID());
       ResponseInputStream i = display.getResponseInputStream();
-      synchronized (i) {
-        i.readReply (o);
-        i.skip (1);
-        enabled = i.readBool ();
-        i.skip (24);
+      synchronized(i) {
+        i.readReply(o);
+        i.skip(1);
+        enabled = i.readBool();
+        i.skip(24);
       }
     }
     return enabled;
@@ -333,25 +356,25 @@ public class Shape extends Extension implements EventFactory {
   public static class RectanglesInfo {
 
     /**
-     * One of {@link #UN_SORTED}, {@link #Y_SORTED}, {@link #YX_SORTED},
-     * {@link #YX_BANDED}
+     * One of {@link Ordering}.
      */
-    public int ordering;
+    private Ordering ordering;
 
-    public Rectangle [] rectangles;
+    private Rectangle [] rectangles;
 
-    RectanglesInfo (ResponseInputStream i) {
-      ordering = i.readInt8 ();
-      i.skip (6);
-      int nrects = i.readInt32 ();
+    RectanglesInfo(ResponseInputStream i) {
+      ordering = Ordering.getByCode(i.readInt8());
+      i.skip(6);
+      int nrects = i.readInt32();
       rectangles = new Rectangle [nrects];
-      i.skip (20);
-      for (int j = 0; j < nrects; j++) {
-        int x = i.readInt16 ();
-        int y = i.readInt16 ();
-        int w = i.readInt16 ();
-        int h = i.readInt16 ();
-        rectangles [j] = new Rectangle (x, y, w, h);
+      i.skip(20);
+      
+      for (Rectangle rec : rectangles) {
+        int x = i.readInt16();
+        int y = i.readInt16();
+        int w = i.readInt16();
+        int h = i.readInt16();
+        rec = new Rectangle(x, y, w, h);
       }
     }
 
@@ -362,19 +385,19 @@ public class Shape extends Extension implements EventFactory {
   /**
    * @see <a href="XShapeGetRectangles.html">XShapeGetRectangles</a>
    */
-  public RectanglesInfo rectangles (Window window, int kind) {
+  public RectanglesInfo rectangles(Window window, Kind kind) {
 
     RectanglesInfo info;
     RequestOutputStream o = display.getResponseOutputStream();
-    synchronized (o) {
-      o.beginRequest (majorOpcode, 7, 3);
-      o.writeInt32 (window.getID());
-      o.writeInt8 (kind);
+    synchronized(o) {
+      o.beginRequest(majorOpcode, 7, 3);
+      o.writeInt32(window.getID());
+      o.writeInt8(kind.getCode());
       ResponseInputStream i = display.getResponseInputStream();
-      synchronized (i) {
-        i.readReply (o);
-        i.skip (1);
-        info = new RectanglesInfo (i);
+      synchronized(i) {
+        i.readReply(o);
+        i.skip(1);
+        info = new RectanglesInfo(i);
       }
     }
     return info;
@@ -385,57 +408,56 @@ public class Shape extends Extension implements EventFactory {
   public static class NotifyEvent extends Event {
     public static final int code = 0;
 
-    public int window_id;
-    public int x;
-    public int y;
-    public int width;
-    public int height;
-    public int time;
-    public boolean shaped;
+    private int windowID;
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private int time;
+    private boolean shaped;
 
-    NotifyEvent (Display display, ResponseInputStream i) { 
-      super (display, i);
-      window_id = i.readInt32 ();
-      x = i.readInt16 ();
-      y = i.readInt16 ();
-      width = i.readInt16 ();
-      height = i.readInt16 ();
-      time = i.readInt32 ();
-      shaped = i.readBool ();
-      i.skip (11);
+    NotifyEvent(Display display, ResponseInputStream i) { 
+      super(display, i);
+      windowID = i.readInt32();
+      x = i.readInt16();
+      y = i.readInt16();
+      width = i.readInt16();
+      height = i.readInt16();
+      time = i.readInt32();
+      shaped = i.readBool();
+      i.skip(11);
     }
 
-    public int x () { return x; }
-    public int y () { return y; }
-    public int width () { return width; }
-    public int height () { return height; }
-    public int time () { return time; }
-    public boolean shaped () { return shaped; }
+    public int x() { return x; }
+    public int y() { return y; }
+    public int width() { return width; }
+    public int height() { return height; }
+    public int time() { return time; }
+    public boolean shaped() { return shaped; }
 
     /**
      * @return valid:
-     * {@link #BOUNDING},
-     * {@link #CLIP}
+     * {@link Kind}
      */
-    public int kind () { return detail; }
+    public Kind kind() { return Kind.getByCode(detail); }
 
 
-    public Rectangle rectangle () {
-      return new Rectangle (x (), y (), width (), height ());
+    public Rectangle rectangle() {
+      return new Rectangle(x(), y(), width(), height());
     }
   }
 
 
-  public Event build (Display display, ResponseInputStream i, int code) {
+  public Event build(Display display, ResponseInputStream i, int code) {
     // only one extension event
-    return new NotifyEvent (display, i);
+    return new NotifyEvent(display, i);
   }
 
 
-  public String moreString () {
+  public String moreString() {
     return "\n  client-version: " 
       + CLIENT_MAJOR_VERSION + "." + CLIENT_MINOR_VERSION
       + "\n  server-version: "
-      + server_major_version + "." + server_minor_version;
+      + serverMajorVersion + "." + serverMinorVersion;
   }
 }
