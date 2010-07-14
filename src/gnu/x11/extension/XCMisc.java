8f0bab94a9f4742d0adedb6eef937e2c31107de3
Marcos Roriz <marcosrorizinf@gmail.com>
2010-07-14 00:41:26 -0300

    XCMist:

--- src/gnu/x11/extension/XCMisc.java
+++ src/gnu/x11/extension/XCMisc.java
@@ -9,7 +9,7 @@ import gnu.x11.ResponseInputStream;
  * >here</a>.
  */
 public class XCMisc extends Extension {
-  public static final String [] MINOR_OPCODE_STRINGS = {
+  public static final String[] MINOR_OPCODE_STRINGS = {
     "GetVersion",               // 0
     "GetXIDRange",              // 1
     "GetXIDList"                // 2
@@ -19,55 +19,56 @@ public class XCMisc extends Extension {
   public static final int CLIENT_MAJOR_VERSION = 1;
   public static final int CLIENT_MINOR_VERSION = 1;
 
-  public int server_major_version, server_minor_version;
+  private int serverMajorVersion, serverMinorVersion;
 
 
   // xc-misc opcode 0 - get version
-  public XCMisc (gnu.x11.Display display) throws NotFoundException {
-    super (display, "XC-MISC", MINOR_OPCODE_STRINGS); 
+  public XCMisc(gnu.x11.Display display) throws NotFoundException {
+    super(display, "XC-MISC", MINOR_OPCODE_STRINGS); 
 
     // check version before any other operations
     RequestOutputStream o = display.getResponseOutputStream();
-    synchronized (o) {
+    synchronized(o) {
 
-      o.beginRequest (majorOpcode, 0, 2);
-      o.writeInt16 (CLIENT_MAJOR_VERSION);
-      o.writeInt16 (CLIENT_MINOR_VERSION);
+      o.beginRequest(majorOpcode, 0, 2);
+      o.writeInt16(CLIENT_MAJOR_VERSION);
+      o.writeInt16(CLIENT_MINOR_VERSION);
+      
       ResponseInputStream i = display.getResponseInputStream();
-      synchronized (i) {
-        i.readReply (o);
-        i.skip (8);
-        server_major_version = i.readInt16 ();
-        server_minor_version = i.readInt16 ();
-        i.skip (20);
+      synchronized(i) {
+        i.readReply(o);
+        i.skip(8);
+        serverMajorVersion = i.readInt16();
+        serverMinorVersion = i.readInt16();
+        i.skip(20);
       }
     }
   }
 
   
-  /** Reply of {@link XCMisc#xid_range()} */
+  /** Reply of {@link XCMisc#xidRange()} */
   public static class XIDRange {
-    public int start_id;
+    public int startID;
     public int count;
-    XIDRange (ResponseInputStream i) {
-      start_id = i.readInt32 ();
-      count = i.readInt32 ();
+    XIDRange(ResponseInputStream i) {
+      startID = i.readInt32();
+      count = i.readInt32();
     }
   }
   
   
   // xc-misc opcode 1 - get xid range
-  public XIDRange xid_range () {
+  public XIDRange xidRange() {
     XIDRange r;
     RequestOutputStream o = display.getResponseOutputStream();
-    synchronized (o) {
-      o.beginRequest (majorOpcode, 1, 1);
+    synchronized(o) {
+      o.beginRequest(majorOpcode, 1, 1);
       ResponseInputStream i = display.getResponseInputStream();
-      synchronized (i) {
-        i.readReply (o);
-        i.skip (8);
-        r = new XIDRange (i);
-        i.skip (16);
+      synchronized(i) {
+        i.readReply(o);
+        i.skip(8);
+        r = new XIDRange(i);
+        i.skip(16);
       }
     }
     return r;
@@ -75,31 +76,32 @@ public class XCMisc extends Extension {
 
 
   // xc-misc opcode 2 - get xid list
-  public int [] xid_list (int count) {
+  public int[] xidList(int count) {
 
     int [] ids;
     RequestOutputStream o = display.getResponseOutputStream();
-    synchronized (o) {
-      o.beginRequest (majorOpcode, 2, 2);
+    synchronized(o) {
+      o.beginRequest(majorOpcode, 2, 2);
+      
       ResponseInputStream i = display.getResponseInputStream();
-      synchronized (i) {
-        i.readReply (o);
-        i.skip (8);
-        int num = i.readInt32 ();
-        i.skip (20);
+      synchronized(i) {
+        i.readReply(o);
+        i.skip(8);
+        int num = i.readInt32();
+        i.skip(20);
         ids = new int [num];
-        for (int j = 0; j < num; j++)
-          ids [j] = i.readInt32 ();
+        for (int resourceID : ids)
+          resourceID = i.readInt32();
       }
     }
     return ids;
   }
 
 
-  public String moreString () {
+  public String moreString() {
     return "\n  client-version: " 
       + CLIENT_MAJOR_VERSION + "." + CLIENT_MINOR_VERSION
       + "\n  server-version: "
-      + server_major_version + "." + server_minor_version;
+      + serverMajorVersion + "." + serverMinorVersion;
   }
 }
