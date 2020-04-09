7da6e63091db60f2cdc446701049d0fd8bf143dd
John Mercier <moaxcp@gmail.com>
2020-04-08 22:24:51 -0400

    Adding test coverage for DisplayName

--- src/main/java/gnu/x11/DisplayName.java
+++ src/main/java/gnu/x11/DisplayName.java
@@ -2,38 +2,45 @@ package gnu.x11;
 
 import lombok.*;
 
-/** X display name. */
-@Value
+import static gnu.util.Strings.*;
+
+/**
+ * X display name.
+ */
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class DisplayName {
-  String hostName;
-  int displayNumber;
-  int screenNumber;
+  final String hostName;
+  final int displayNumber;
+  final int screenNumber;
 
   public static DisplayName parse(@NonNull String convention) {
-    int i = convention.indexOf (':');
+    requiresNonBlank("convention", convention);
+    int i = convention.indexOf(':');
 
     // case 1: convention = hostName
     if (i == -1) {
       return new DisplayName(convention, 0, 0);
     }
 
-    String hostName = convention.substring (0, i);
-    int j = convention.indexOf ('.', i);
+    String hostName = i == 0 ? null : convention.substring(0, i);
+    int j = convention.indexOf('.', i);
 
     // case 2: convention = hostName:displayNumber
     if (j == -1) {
-      int displayNumber = Integer.parseInt(convention.substring (i + 1));
+      int displayNumber = Integer.parseInt(convention.substring(i + 1));
       return new DisplayName(hostName, displayNumber, 0);
     }
 
     // case 3: convention = hostName:displayNumber.screenNumber
-    int displayNumber = Integer.parseInt(convention.substring (i + 1, j));
-    int screenNumber = Integer.parseInt(convention.substring (j + 1));
+    int displayNumber = Integer.parseInt(convention.substring(i + 1, j));
+    int screenNumber = Integer.parseInt(convention.substring(j + 1));
     return new DisplayName(hostName, displayNumber, screenNumber);
   }
 
 
-  public String toString () {
-    return hostName + ":" + displayNumber + "." + screenNumber;
+  public String toString() {
+    String h = hostName == null ? "" : hostName;
+    return h + ":" + displayNumber + "." + screenNumber;
   }
 }
