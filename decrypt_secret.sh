ec99ce6b1c754940d6558ec6ea9db1699b962444
John Mercier <moaxcp@gmail.com>
2020-04-19 10:35:05 -0400

    debugging github actions

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
+mkdir $HOME/secrets || true
 # --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$ENCRYPTED_SIGNING_PASSPHRASE" \
+gpg --quiet --batch --yes --decrypt --passphrase="${ENCRYPTED_SIGNING_PASSPHRASE}" \
 --output $HOME/secrets/signingkey.gpg signingkey.gpg.gpg
