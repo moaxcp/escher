505e50c5d6863428c73e2d087bebbb11cd64e8f8
John Mercier <moaxcp@gmail.com>
2020-05-09 14:37:27 -0400

    Adding test for init of Display

--- src/test/java/publicapi/display/DisplayConstructor.java
+++ src/test/java/publicapi/display/DisplayConstructor.java
@@ -1,8 +1,10 @@
 package publicapi.display;
 
-import gnu.x11.Display;
-import gnu.x11.X11ClientException;
+import gnu.x11.*;
+import java.io.*;
 import java.net.*;
+import java.util.Optional;
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import mockit.*;
 import org.junit.jupiter.api.Test;
 
@@ -30,5 +32,32 @@ public class DisplayConstructor {
     };
     X11ClientException exception = assertThrows(X11ClientException.class, () -> new Display(socket, 0, 0));
     assertThat(exception).hasCause(cause);
+  }
+
+  @Test
+  @Ignore
+  void constructor(@Injectable Socket socket, @Injectable InetAddress inetAddress, @Injectable XAuthority xAuthority, @Injectable InputStream in, @Injectable OutputStream out) throws IOException {
+    new Expectations() {{
+      socket.getInetAddress().getHostName(); result = "localhost";
+      socket.getInputStream(); result = in;
+      socket.getOutputStream(); result = out;
+      inetAddress.getHostName(); result = "n1";
+      xAuthority.getProtocolName(); result = "MIT-MAGIC-COOKIE-1";
+      xAuthority.getProtocolData(); result = new byte[] { 1, 2, 3 };
+    }};
+    new MockUp<InetAddress>() {
+      @Mock
+      InetAddress getLocalHost() {
+        return inetAddress;
+      }
+    };
+    new MockUp<XAuthority>() {
+      @Mock
+      Optional<XAuthority> getAuthority(String hostName) {
+        return Optional.of(xAuthority);
+      }
+    };
+
+    new Display(socket, 1, 2);
   }
 }
